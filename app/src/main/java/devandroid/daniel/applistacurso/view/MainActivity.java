a75e0cd31ffa9888f157622e15aaa1190b531f77
danielrodriguesdev <63358147+danielrodriguesdev@users.noreply.github.com>
2023-10-23 15:53:28 -0300

    Refatorado Layout para LinearLayout

--- app/src/main/java/devandroid/daniel/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/daniel/applistacurso/view/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         editPrimeiroNome = findViewById(R.id.editPrimeiroNome);
         editSegundoNome = findViewById(R.id.editSegundoNome);
